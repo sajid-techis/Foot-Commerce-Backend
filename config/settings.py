7507f8576d62f5d7788a8a490924117631d0d81d
Bhat Sajid <89973801+sajid-techis@users.noreply.github.com>
2022-09-24 23:58:02 +0000

    Url's Updated

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0:3000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0:3000','https://foot-commerce-backend.sajid-techis.repl.co/']
 
 # Application definition
 
